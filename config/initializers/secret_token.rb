5e49305980cbfb78fd341735fdc695abd539d891
Payup.me <contact@payup.me>
2012-06-04 02:09:03 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PayupMe::Application.config.secret_token = '8f0974b7b6652e8032b6abfcc7bbadda5b4d2dca504c9e65aad8fd7d0da65cd262f9c6f7120b76c4160b0566c2a1f0843d6da0d5b0c5575513e4330a72025788'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PayupMe::Application.config.session_store :cookie_store, key: '_payup.me_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PayupMe::Application.config.session_store :active_record_store
